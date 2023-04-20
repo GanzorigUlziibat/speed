fea92e0b2159198cde13ba40a05f3bc8ee8c37eb
hhhooonnngggooorrrzzzuuulll <hongorzulrose@gmail.com>
2023-04-20 20:20:10 +0800

    aboutscreen

--- speed/src/screen/AboutScreen.js
+++ speed/src/screen/AboutScreen.js
@@ -1,24 +1,30 @@
-import { View, Text, SafeAreaView, Image,StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function AboutScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={require('../../assets/speed.png')} style={styles.img}/>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
+      <View style={styles.view1}>
+        <Image source={require('../../assets/speed.png')} style={styles.img} />
+        <Text>МАНДАХ ИХ СУРГУУЛЬ</Text>
+        <Text>Мэдээлэл, Технологийн Сургууль</Text>
+        <Text>Мандах их сургуулийн Мэдээлэл, Технологийн сургуулийн багш, оюутануудын хамтын бүтээл юм. Дараа дараагийн нэмэлт сайжруулалтыг элсэгч, дүү нар нь сайжруулан хөгжүүлж байна.</Text>
+        <Text>Бүх эрх хуулиар хамгаалагдсан @2023</Text>
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  img:{
-    width: 300,
-    height: 300,
+  view1:{
+    alignItems:"center",
+  },
+  img: {
+    width: 250,
+    height: 250,
+    borderRadius: 500,
   }
 })
