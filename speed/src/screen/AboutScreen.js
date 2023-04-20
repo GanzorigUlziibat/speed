60cd829852fdbdd8e2d7c2c322e6d443c3a49be1
hhhooonnngggooorrrzzzuuulll <hongorzulrose@gmail.com>
2023-04-20 20:09:20 +0800

    img

--- speed/src/screen/AboutScreen.js
+++ speed/src/screen/AboutScreen.js
@@ -1,10 +1,24 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image,StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function AboutScreen() {
   return (
-    <SafeAreaView>
-      <Text>AboutScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('../../assets/speed.png')} style={styles.img}/>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  img:{
+    width: 300,
+    height: 300,
+  }
+})
